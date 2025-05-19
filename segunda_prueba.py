39d01d439563d540f15267428498aef03036e282
pabloom96 <ortizmontufar.pablo@gmail.com>
2025-05-19 10:15:59 -0600

    Con keybind de forrmato

--- segunda_prueba.py
+++ segunda_prueba.py
@@ -2,3 +2,8 @@ print('Segunda prueba jiji')
 print()
 print()
 print('En los notebooks, ejecutar es Ctrl+Enter, aquí es Shift+Enter')
+#print(Prueba de error, linitng)
+x = 10
+if x % 2 == 0:
+print('x es non')
+print('Shift+Ctrl+P --> Format Document, esto corrige todos los errores de formato del documento como indexaciones erroneas')
