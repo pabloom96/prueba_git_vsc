5633d1405d098680a1ce15ec67a3bc4e7984f1e6
pabloom96 <ortizmontufar.pablo@gmail.com>
2025-05-19 11:03:13 -0600

    hola xd

--- segunda_prueba.py
+++ segunda_prueba.py
@@ -2,8 +2,9 @@ print('Segunda prueba jiji')
 print()
 print()
 print('En los notebooks, ejecutar es Ctrl+Enter, aquí es Shift+Enter')
-#print(Prueba de error, linitng)
+# print(Prueba de error, linitng)
 x = 10
 if x % 2 == 0:
 print('x es non')
 print('Shift+Ctrl+P --> Format Document, esto corrige todos los errores de formato del documento como indexaciones erroneas')
+print('Ultimo hola')
